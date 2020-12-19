c7cc007151f05c223ca72b6ef29d0470953f505a
nikshingadiya <shingadiyanikhil1634@gmail.com>
2020-12-19 18:13:02 +0530

    web programming
    html ,css and js start

--- HTML/javascrpt1.js
+++ HTML/javascrpt1.js
@@ -1,4 +1,31 @@
 console.log('hello world');
-var1  = "rt"; var2 = 20;
-console.log(var1+var2)
+var1  = 10; var2 = 20;
+console.log(var1+2)
 console.log("Nikhil")
+if(var1>30)
+{
+    console.log("Nikhil")
+}
+else
+{
+    console.log('good morning')
+}
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(typeof(person));
+console.log(person);
+
+function myfunction(p1,p2)
+{
+    return p1*p2;
+}
+var x = myfunction(4, 3);
+console.log(x)
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
