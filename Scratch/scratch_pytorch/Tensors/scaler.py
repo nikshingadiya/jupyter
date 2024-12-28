83236ec98bb8c795f75c4b47c7b4ae83a9911233
nikshingadiya <shingadiyanikhil1634@gmail.com>
2024-12-28 11:08:47 +0530

    tensor introduction completed

---
+++ Scratch/scratch_pytorch/Tensors/scaler.py
@@ -0,0 +1,10 @@
+import torch
+
+torch0d=torch.tensor(1)
+
+torch01d=torch.tensor([1,2])
+
+torch02d=torch.tensor([[1,2],[3,4]])
+print(torch0d)
+print(torch01d)
+print(torch02d)
---
+++ Scratch/scratch_pytorch/Tensors/type_scaler.py
@@ -0,0 +1,6 @@
+import torch
+
+x=torch.tensor([1.0,2.0])
+print(x.dtype)
+tensor1d = torch.tensor([1, 2, 3])
+print(tensor1d.dtype) # torch.int64
